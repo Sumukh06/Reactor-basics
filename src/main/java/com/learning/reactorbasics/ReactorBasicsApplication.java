2ad5b2239fe8f923e0b0ef128055a65c888db58a
Sumukh <sumukhn06@gmail.com>
2022-02-10 11:31:12 +0530

    init

---
+++ src/main/java/com/learning/reactorbasics/ReactorBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.reactorbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorBasicsApplication.class, args);
+	}
+
+}
